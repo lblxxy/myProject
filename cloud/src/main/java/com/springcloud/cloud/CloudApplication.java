60b3b0239b48d57048e8208b0674260821fe1a91
bl <1357961823@qq.com>
2019-03-14 17:27:18 +0800

    dev

--- cloud/src/main/java/com/springcloud/cloud/CloudApplication.java
+++ cloud/src/main/java/com/springcloud/cloud/CloudApplication.java
@@ -1,13 +1,21 @@
 package com.springcloud.cloud;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudApplication.class, args);
     }
+
 
 }
