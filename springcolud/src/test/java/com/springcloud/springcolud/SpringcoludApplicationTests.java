ca1520f8fe769e89f6fd18cc74924ab47fec8abb
bl <1357961823@qq.com>
2019-03-15 10:22:00 +0800

    firstCloud

---
+++ springcolud/src/test/java/com/springcloud/springcolud/SpringcoludApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcloud.springcolud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcoludApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
