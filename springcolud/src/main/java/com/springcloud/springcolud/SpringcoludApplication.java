9b314cd35c8b23d5f349bcb5ce8890f420432020
bl <1357961823@qq.com>
2019-03-15 10:22:00 +0800

    firstCloud

---
+++ springcolud/src/main/java/com/springcloud/springcolud/SpringcoludApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.springcolud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoludApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcoludApplication.class, args);
+    }
+
+}
