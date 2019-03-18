0f405cb37c6f273eed49ca61ffa82c03423d60b2
bl <1357961823@qq.com>
2019-03-18 16:39:08 +0800

    ribbon

--- springcloud/ribbon/src/main/java/com/lbl/ribbon/service/HelloService.java
+++ springcloud/ribbon/src/main/java/com/lbl/ribbon/service/HelloService.java
@@ -1,6 +1,5 @@
 package com.lbl.ribbon.service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,14 +10,8 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
-        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
-    }
-
-    public String hiError(String name) {
-        System.out.println("进来了！！！");
-        return "hi,"+name+",sorry,error!";
+        return restTemplate.getForObject("http://service-hi/hi?name="+name,String.class);
     }
 
 }
