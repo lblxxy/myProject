a3bf353830af149748b5d58fdb3712822ff22cbb
bl <1357961823@qq.com>
2019-03-21 17:09:50 +0800

    configServer配置测试

---
+++ config-client/src/main/java/com/lbl/configclient/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.lbl.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    //远程配置文件里的参数
+    @Value("${foo}")
+    String key;
+
+    @RequestMapping(value = "/hi")
+    public String cfgTest(){
+        return "Config Param Is：" + key;
+    }
+}
