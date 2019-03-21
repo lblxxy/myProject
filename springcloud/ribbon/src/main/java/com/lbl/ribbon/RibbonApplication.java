65972aa07370a37da6c83f00aa181d2c2be6cf41
bl <1357961823@qq.com>
2019-03-21 14:01:38 +0800

    ribbon

--- springcloud/ribbon/src/main/java/com/lbl/ribbon/RibbonApplication.java
+++ springcloud/ribbon/src/main/java/com/lbl/ribbon/RibbonApplication.java
@@ -1,11 +1,14 @@
 package com.lbl.ribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class RibbonApplication {
 
     public static void main(String[] args) {
@@ -23,5 +27,22 @@ public class RibbonApplication {
     @LoadBalanced
     RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    /**
+     * 在2.0版本下需要在启动类中添加如下配置才可以访问断路器仪表盘
+     * 访问url为：http://localhost:8764/hystrix
+     * 在页面填写路径： http://localhost:8764/hystrix.stream
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
