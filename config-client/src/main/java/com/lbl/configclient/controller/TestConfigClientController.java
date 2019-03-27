2a5fcc77974236a9e471218e92804004ee5f74ce
bl <1357961823@qq.com>
2019-03-27 13:35:22 +0800

    config-server

--- config-client/src/main/java/com/lbl/configclient/controller/TestConfigClientController.java
+++ config-client/src/main/java/com/lbl/configclient/controller/TestConfigClientController.java
@@ -1,10 +1,13 @@
 package com.lbl.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //需要在配置的页面加上，就是说附带@Value的页面加上此注解
 public class TestConfigClientController {
     @Value("${foo}")
     String foo;
@@ -12,4 +15,9 @@ public class TestConfigClientController {
     public String hi(){
         return foo;
     }
+    @PostMapping("/actuator/bus-refresh")
+    public void busRefresh(){
+        System.out.println("foo:"+foo);
+    }
+
 }
