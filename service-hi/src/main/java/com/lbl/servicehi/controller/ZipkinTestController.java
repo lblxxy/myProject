435efa965e0546597532f9c174de3ce7e937a801
bl <1357961823@qq.com>
2019-03-27 14:46:52 +0800

    zipkin服务链路追踪

---
+++ service-hi/src/main/java/com/lbl/servicehi/controller/ZipkinTestController.java
@@ -0,0 +1,44 @@
+package com.lbl.servicehi.controller;
+
+import brave.sampler.Sampler;
+import com.lbl.servicehi.ServiceHiApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+public class ZipkinTestController {
+
+    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:6070/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+}
