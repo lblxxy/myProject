a65f4fe55a05a355fb39fe79d396745ceb9c37a3
bl <1357961823@qq.com>
2019-04-24 11:55:48 +0800

    雏形

---
+++ myframework/src/main/java/com/bl/myframework/MyframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.bl.myframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyframeworkApplication.class, args);
+    }
+
+}
